1fe73e3d4b6fffb8253da8d1ed2cc724d5c1611f
Pritam Sethuraman <pritamsethuraman@gmail.com>
2026-07-29 11:05:48 -0400

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth;
 
 export const config = {
-  matcher: ["/register", "/admin"],
+  matcher: ["/admin/*", "/(app)/*"],
 };
 
 // import { NextResponse } from "next/server";

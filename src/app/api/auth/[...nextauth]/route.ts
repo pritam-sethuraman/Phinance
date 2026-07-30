8a3370004876fff30afd18798179caf41dc7ced0
Pritam Sethuraman <pritamsethuraman@gmail.com>
2026-07-30 00:39:16 -0400

    updated auth api

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,4 +5,6 @@
 
 // export { handler as GET, handler as POST };
 
-export { GET, POST } from "@/lib/auth/auth";
+import { handlers } from "@/lib/auth/auth";
+
+export const { GET, POST } = handlers;
